14d8a76e4631507c4e9b2b3162ad910399a38d48
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2023-02-05 23:28:38 +0200

    cleanup + add main menu scene

--- src/gamestate.gd
+++ src/gamestate.gd
@@ -1,5 +1,19 @@
 extends Node
 
+var levels = [
+	load("res://src/level_01/Level_01.tscn"),
+	load("res://src/level_02/Level_02.tscn")
+]
+
 var state: Dictionary = {
 	"first_time_dwarf": true
 }
+
+func load_game() -> void:
+	# TODO: Load progress?
+	load_level(1)
+
+func load_level(id: int, spawn_coords: Vector2 = Vector2.ZERO) -> void:
+	get_tree().change_scene_to(levels[id - 1])
+	if (spawn_coords != Vector2.ZERO):
+		pass
---
+++ src/mainmenu/MainMenu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_StartBTN_pressed():
+	Gamestate.load_game()
