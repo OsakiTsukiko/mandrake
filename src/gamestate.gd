07bfcf440b728b9f24eef944f5c39efc56c3135d
serb2331 <serban.foica@gmail.com>
2023-02-09 09:03:25 +0200

    init settings

--- src/gamestate.gd
+++ src/gamestate.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var settings_scene: Resource = load("res://src/optionsmenu/OptionsMenu.tscn")
+
 var levels = [
 	load("res://src/level_01/Level_01.tscn"),
 	load("res://src/level_02/Level_02.tscn")
@@ -15,3 +17,6 @@ func load_level(id: int, spawn_coords: Vector2 = Vector2.ZERO) -> void:
 	get_tree().change_scene_to(levels[id - 1])
 	if (spawn_coords != Vector2.ZERO):
 		call_deferred("emit_signal", "spawn_coords", spawn_coords)
+
+func load_settings_menu():
+	get_tree().change_scene_to(settings_scene)
--- src/mainmenu/MainMenu.gd
+++ src/mainmenu/MainMenu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_StartBTN_pressed():
 	Gamestate.load_level(1, Vector2(0, 20))
+
+func _on_OptionsBTN_pressed():
+	get_tree().change_scene()
