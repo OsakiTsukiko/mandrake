df76507c332e21d2270ae5d177e31f0a884db1f0
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2023-02-12 08:34:09 +0200

    update main menu

--- src/mainmenu/MainMenu.gd
+++ src/mainmenu/MainMenu.gd
@@ -1,22 +1,25 @@
 extends Control
 
-onready var start_btn = $StartBTN
+onready var connect_btn = $ContinueBTN
 
 func _ready() -> void:
 	randomize()
 	if (PlayerManager.last_level != -1):
-		start_btn.text = "Continue"
+		connect_btn.disabled = false
 	else:
-		start_btn.text = "Start"
+		connect_btn.disabled = true
 
 func _on_StartBTN_pressed() -> void:
-	if (PlayerManager.last_level != -1):
-		if (PlayerManager.last_level == 0):
-			Gamestate.load_level(0, Vector2(18, 21))
-		if (PlayerManager.last_level == 1):
-			Gamestate.load_level(1, Vector2(32, 29))
-	else:
-		Gamestate.load_level(0, Vector2(0, 20))
+	PlayerManager.death_reset()
+	Gamestate.state.has_book = false
+	Gamestate.state.first_time_dwarf = true
+	Gamestate.load_level(0, Vector2(0, 20))
 
 func _on_OptionsBTN_pressed():
 	Gamestate.load_settings_menu()
+
+func _on_ContinueBTN_pressed():
+	if (PlayerManager.last_level == 0):
+		Gamestate.load_level(0, Vector2(18, 21))
+	if (PlayerManager.last_level == 1):
+		Gamestate.load_level(1, Vector2(32, 29))
